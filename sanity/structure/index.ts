65db472e4efe370f6fd9540d71b2b21d05e8c1b2
Christoph Khouri <christoph.khouri@gmail.com>
2025-06-05 19:06:07 -0400

    Adding . apiVersion to all calls
    Fixes this warning:
    
    No apiVersion specified for document type list with custom filter: ... This will be required in the future. See https://docs.sanity.io/help/structure-api-version-required-for-custom-filter for more info.

--- sanity/structure/index.ts
+++ sanity/structure/index.ts
@@ -18,28 +18,40 @@ export const structure: StructureResolver = (S) =>
                 .schemaType('car')
                 .icon(() => '🚙')
                 .child(
-                  S.documentList().title('Available Cars').filter('_type == "car" && availabilityStatus == "available"')
+                  S.documentList()
+                    .title('Available Cars')
+                    .filter('_type == "car" && availabilityStatus == "available"')
+                    .apiVersion('2024-03-13')
                 ),
               S.listItem()
                 .title('Sold Cars')
                 .schemaType('car')
                 .icon(() => '🚗')
                 .child(
-                  S.documentList().title('Sold Cars').filter('_type == "car" && availabilityStatus == "sold"')
+                  S.documentList()
+                    .title('Sold Cars')
+                    .filter('_type == "car" && availabilityStatus == "sold"')
+                    .apiVersion('2024-03-13')
                 ),
               S.listItem()
                 .title('Pending Cars')
                 .schemaType('car')
                 .icon(() => '⏳')
                 .child(
-                  S.documentList().title('Pending Cars').filter('_type == "car" && availabilityStatus == "pending"')
+                  S.documentList()
+                    .title('Pending Cars')
+                    .filter('_type == "car" && availabilityStatus == "pending"')
+                    .apiVersion('2024-03-13')
                 ),
               S.listItem()
                 .title('All Cars')
                 .schemaType('car')
                 .icon(() => '🚗')
                 .child(
-                  S.documentList().title('All Cars').filter('_type == "car"')
+                  S.documentList()
+                    .title('All Cars')
+                    .filter('_type == "car"')
+                    .apiVersion('2024-03-13')
                 ),
             ])
         ),
@@ -57,35 +69,50 @@ export const structure: StructureResolver = (S) =>
                 .schemaType('page')
                 .icon(() => '🏠')
                 .child(
-                  S.documentList().title('Landing Page').filter('_type == "page" && pageType == "landing"')
+                  S.documentList()
+                    .title('Landing Page')
+                    .filter('_type == "page" && pageType == "landing"')
+                    .apiVersion('2024-03-13')
                 ),
               S.listItem()
                 .title('About Pages')
                 .schemaType('page')
                 .icon(() => '👥')
                 .child(
-                  S.documentList().title('About Pages').filter('_type == "page" && pageType == "about"')
+                  S.documentList()
+                    .title('About Pages')
+                    .filter('_type == "page" && pageType == "about"')
+                    .apiVersion('2024-03-13')
                 ),
               S.listItem()
                 .title('Contact Pages')
                 .schemaType('page')
                 .icon(() => '📞')
                 .child(
-                  S.documentList().title('Contact Pages').filter('_type == "page" && pageType == "contact"')
+                  S.documentList()
+                    .title('Contact Pages')
+                    .filter('_type == "page" && pageType == "contact"')
+                    .apiVersion('2024-03-13')
                 ),
               S.listItem()
                 .title('Article Pages')
                 .schemaType('page')
                 .icon(() => '📝')
                 .child(
-                  S.documentList().title('Article Pages').filter('_type == "page" && pageType == "article"')
+                  S.documentList()
+                    .title('Article Pages')
+                    .filter('_type == "page" && pageType == "article"')
+                    .apiVersion('2024-03-13')
                 ),
               S.listItem()
                 .title('General Pages')
                 .schemaType('page')
                 .icon(() => '📄')
                 .child(
-                  S.documentList().title('General Pages').filter('_type == "page" && pageType == "general"')
+                  S.documentList()
+                    .title('General Pages')
+                    .filter('_type == "page" && pageType == "general"')
+                    .apiVersion('2024-03-13')
                 ),
             ])
         ),
